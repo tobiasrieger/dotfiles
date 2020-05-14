082afe28df4c5f6b53e44a6f665031a98358cece
Tobias Rieger <tobias_rieger@icloud.com>
2020-05-14 21:52:32 +0200

    Refactored files

---
+++ zsh_linux/update-configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Add config for zsh
+cp zshrc ~/.zshrc
+# Add theme for ohmyzsh
+cp ../common_files/r3configure.zsh-theme ~/.oh-my-zsh/themes/r3configure.zsh-theme
+# Add conda rc 
+cp ../common_files/condarc ~/.condarc
--- zsh_macos/update-configs.sh
+++ zsh_macos/update-configs.sh
@@ -4,6 +4,7 @@ touch ~/.hushlogin
 # Add config for zsh
 cp zshrc ~/.zshrc
 # Add theme for ohmyzsh
-cp r3configure.zsh-theme ~/.oh-my-zsh/themes/r3configure.zsh-theme
+cp ../common_files/r3configure.zsh-theme ~/.oh-my-zsh/themes/r3configure.zsh-theme
 # Add conda rc 
-cp condarc ~/.condarc
+cp ../common_files/condarc ~/.condarc
+
