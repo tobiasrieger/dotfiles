02ab86785fc4e0e843e471b115172ca5cea70fd0
Tobias Rieger <tobias_rieger@icloud.com>
2020-07-24 10:31:04 +0100

    Added support for the raspberry pi

---
+++ manjaro/update-configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Add config for zsh
+cp zshrc ~/.zshrc
+# Add theme for ohmyzsh
+cp ../common_files/r3configure.zsh-theme ~/.oh-my-zsh/themes/r3configure.zsh-theme
+# Add conda rc 
+cp ../common_files/condarc ~/.condarc
+# Add conky rc
+cp conkyrc ~/.conkyrc
